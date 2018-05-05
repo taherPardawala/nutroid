2d515c576f5b6702ac7e255141b033c2dab4cd6e
786kaizersoze <dhamapurkar54@gmail.com>
2018-05-05 13:42:18 +0530

    created NPM package

--- test-framework/user.js
+++ test-framework/user.js
@@ -1,6 +1,6 @@
-let instance = require('./client')({url:"http://localhost:3000",userId:'userid'});
+let instance = require('rtsocketclient')({url:"http://localhost:3000",userId:'userid'});
 let client = instance.defineClient();
-client.say("asdasd");
+client.say("HI!");
 
 instance.on('text',function(data){
     console.log("YAY",data);
